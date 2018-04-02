44ecae5021a03360e32628decd7f936b3048d6bf
Juulpy <38010280+Juulpy@users.noreply.github.com>
2018-04-02 21:11:18 +0200

    v0.11

--- main/PythonAutoTyper.py
+++ main/PythonAutoTyper.py
@@ -3,26 +3,25 @@ import pyautogui as typer
 import time
 
 #Starting lines
-print('Vocabtyper v0.1 by Petit, made in Goutum')
+print('Vocabtyper v0.11 by me, made in Goutum')
 print('==================================' )
 print('')
-print('Werkt momenteel alleen voor Engels omdat Python Franse tekens niet herkent in de textfiles.')
-print('')
-print("Zorg dat je de file van je vocab in goede volgorde hebt, en dat het txt-bestandje 'vocab' heet")
-print("Als het hele textbestand is getypt, start dit programma opnieuw op. (Wordt later automatisch)")
-time.sleep(1)
 
-#Ask for interval
-interval = input('Interval tussen letters (seconden): ')
-print('In 5 seconden start de typer')
+#Ask and print interval
+interval = input('Interval tussen karakters (seconden): ')
+print(interval)
+print('')
+#Ask and print delay
+delay = input('Aantal seconden na de eerstvolgende entertoets dat de autotyper gaat typen: ')
+print('Na',(delay),'seconden start de typer')
 
 #import file
-text_file = open('vocab.txt', "r")
-text_file.close()
+#text_file = open('vocab.txt', "r")
+#text_file.close()
 
 #Count number of lines as num_lines
 num_lines = sum(1 for line in open('vocab.txt'))
-print('Aantal lines: ',num_lines)
+print('Aantal lines: ',num_lines * 2)
 
 #Read text file
 text_file = open('vocab.txt', "r")
@@ -34,6 +33,6 @@ time.sleep(5)
 
 #Auto-typer ('Text', delay=[var]')
 typer.typewrite((a), interval=(interval))
+typer.typewrite((a), interval=(interval))
 
-
-
+print('klaar')
